d3da00636383a90012ec94017a2a4cd83f0920a2
zurgeg <zurgeg@gmail.com>
2021-06-22 10:07:20 -0400

    Allow for the usage of slash commands

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 from discord.ext.commands import Bot
 from dotenv import load_dotenv
 from os import environ
+from discord_slash import SlashCommand
 
 load_dotenv()
 
 bot = Bot()
+slash = SlashCommand(bot, sync_commands=True)
 
 bot.run(environ['WARP_TOKEN'])
