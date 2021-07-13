3ef0071f80571b28c772e716b4a1de942cd84be0
zurgeg <zurgeg@gmail.com>
2021-07-13 16:37:00 -0400

    b o n k

---
+++ fun.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+from discord_slash import cog_ext
+
+class Fun(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    def bonk(self, ctx):
+        ctx.send("bonk")     
+    
+    @commands.command(name='bonk')
+    def bonk_normal(self, ctx):
+        self.bonk(ctx)
+    
+    @cog_ext.cog_slash(name="bonk")
+    def bonk_slash(self, ctx):
+        self.bonk(ctx)
