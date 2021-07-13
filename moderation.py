ef64968781feb13cb1c63a9572c00961139a9442
zurgeg <zurgeg@gmail.com>
2021-07-13 16:33:18 -0400

    Moderation + gitignore updates

--- moderation.py
+++ moderation.py
@@ -16,16 +16,17 @@ class Moderation(commands.Cog):
                 await ctx.guild.ban(member)
                 await ctx.send("Banned {}".format(member), hidden=True)
             else:
+                await ctx.send("You do not have permission to ban members", hidden=True)      
+        else: 
+            if ctx.author.guild_permissions.ban_members:
+                await ctx.guild.ban(member)
+                await ctx.send("Banned {}".format(member), hidden=True)
+            else:
                 await ctx.send("You do not have permission to ban members", hidden=True)       
-        if ctx.author.guild_permissions.ban_members:
-            await ctx.guild.ban(member)
-            await ctx.send("Banned {}".format(member), hidden=True)
-        else:
-            await ctx.send("You do not have permission to ban members", hidden=True)       
     @commands.command(name="ban", category="Moderation")
     async def ban_normal(self, ctx, member: User):
         await self.ban(ctx, member)
-    @cog_ext.cog_slash(name="ban", guild_ids=[842948029245161492])
+    @cog_ext.cog_slash(name="ban")
     async def ban_slash(self, ctx, member: User):
         await self.ban(ctx, member)
 
